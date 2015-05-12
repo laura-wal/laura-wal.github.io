35a2b73076a85548d0ed647217d2d42365e1466e
Laura Walpole <l.a.walpole@googlemail.com>
2015-05-12 11:21:31 +0100

    on click working

--- jquery.js
+++ jquery.js
@@ -1,3 +1,51 @@
 $(document).ready(function() {
   console.log('readytoroll');
+
+    // var x = 1
+    // var o = 2
+    // // var player1 = 
+    // // var player2 = 
+
+    // var row 1 = [1, 2, 3]
+    // var row 2 = [4 ,5 ,6]
+    // var row 3 = [7, 8, 9]
+
+//click function for the different table posistions
+  $('#square1').on('click', function(){
+   return console.log("hello1");
+    });
+
+  $('#square2').on('click', function(){
+   return console.log("hello2");
+    });
+
+  $('#square3').on('click', function(){
+   return console.log("hello3");
+    });
+
+  $('#square4').on('click', function(){
+   return console.log("hello4");
+    });
+
+  $('#square5').on('click', function(){
+   return console.log("hello5");
+    });
+
+  $('#square6').on('click', function(){
+   return console.log("hello6");
+    });
+
+  $('#square7').on('click', function(){
+   return console.log("hello7");
+    });
+
+  $('#square8').on('click', function(){
+   return console.log("hello8");
+    });
+
+  $('#square9').on('click', function(){
+   return console.log("hello9");
+    });
+
+
 });
