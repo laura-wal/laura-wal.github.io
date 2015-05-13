e2307c2a8f8243d93ee9c61d1cf77d8d00857f93
Laura Walpole <l.a.walpole@googlemail.com>
2015-05-13 16:07:02 +0100

    winner works

--- jquery.js
+++ jquery.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var player1 = true;
   var player2 = false;
   board = ["", "", "", "", "", "", "", "", ""];
-  winningCombo = [
+  var winningCombo = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,60 +14,34 @@ $(document).ready(function() {
     [0, 4, 8]
     ];
 
-
-  var checkForWin = function(){
-    // debugger;
-      $.each(winningCombo, function(index, win_array){
-        console.log(index, win_array);
-
-     
-    })
+  var checkForWin = function(){ 
+    $.each(winningCombo, function(index, win_array){ 
+      if(board[win_array[0]] == board[win_array[1]] && board[win_array[1]] ==  board[win_array[2]] && board[win_array[0]] !== '') { 
+        console.log('I am being run!');
+        $('div.result').text('You have won!');
+        console.log('win')
+      } else {
+        console.log('draw')
+      }
+    });
   }
 
-
-
 //click function for the different table posistions dependant on the player
-    $('.box').on('click', function(){
-     console.log("hello1", player1, player2);
-     if (player1) {
-     
-        $(this).addClass('cross');
+  $('.box').on('click', function(){
+   //  console.log("hello1", player1, player2);
+    if (player1) { 
+      $(this).addClass('cross');
         player1 = false;
         player2 = true;
+        var cell = $(this).attr('id')
         board[$(this).attr('id')]="x";
-        checkForWin();
-      } else { 
-        $(this).addClass('circle');
+        checkForWin(cell);
+    } else { 
+        $(this).addClass('circle'); 
         player1 = true;
         player2 = false;
         board[$(this).attr('id')]="o";
         checkForWin();
       }
     });
-
-
-// //check if a win has happened
-
-  //   var winner = false;
-  //   for (var i = 0; i < check.length; i++) {
-  //       if (arr.indexOf(check[i]) > -1) {
-  //           found = true;
-  //           break;
-  //       }
-  //   }
-  // }
-
-
-  //_.contains([1, 2, 3], 3);
-  //_.each(list, iteratee, [context])
- 
-  
-
-
-
-
-  //alert winner
- 
-
-
-});
+  });
